8a7dd8c00d2276a0e32477086cedc2918bfb1991
Toucher-F <lianduanb@gmail.com>
2018-02-05 16:32:15 -0500

    add set retire and unretire, fix artist count, add more artists

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -13,7 +13,7 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
   // Write a query that will follow sort, offset, limit options only
   // do not worry about 'criteria' yet
 
-  console.log(criteria);
+  //console.log(criteria);
 
   // const sortOrder = {};
   // sortOrder[sortProperty] = 1;
@@ -22,14 +22,16 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
     .skip(offset)
     .limit(limit);
 
-  return Promise.all([query, Artist.count()]).then(results => {
-    return {
-      all: results[0],
-      count: results[1],
-      offset: offset,
-      limit: limit
-    };
-  });
+  return Promise.all([query, Artist.find(buildQuery(criteria)).count()]).then(
+    results => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset: offset,
+        limit: limit
+      };
+    }
+  );
 };
 
 const buildQuery = criteria => {
